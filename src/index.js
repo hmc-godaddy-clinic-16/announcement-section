8b7762437695cfd48e99bd4535a3163bd477d63b
Maureen Naval <maureennaval@WL-194-26.WPA.Claremont.Edu>
2016-10-12 19:47:02 -0700

    Announcement component created
    Outer announcement component for background with inner content component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-var titleAndContent = React.createClass({
-	<div>
-		<h1 id="title">Title</h1>
-		<p id="content">Content</p>
-	</div>
+var announcementData = {
+	title: "BUY ONE DOZEN GET ONE DOZEN FREE",
+	content: "<b>October 13 - October 19</b> <br> 8:00 AM - 10:00 PM <br> Exclusions apply. See <a href=\"http:www.krispykreme.com\">here<a> for more details.",
+	image: "http://cdn.jamieoliver.com/recipe-database/oldImages/xtra_med/1235_1_1436889055.jpg"
+};
+
+var Announcement = React.createClass({
+	render: function() {
+		var image = this.props.data.image;
+
+		var announcementStyle = {
+			'border-style': 'solid',
+			'background-image': 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(' + image + ')',		
+			'background-repeat': 'no-repeat',
+			'background-size': 'cover'
+		};
+
+		return <div style={announcementStyle}>
+			<Content data={this.props.data} />
+		</div>
+	}
 });
 
-// var Announcement = React.createClass({
-// 	render: function() {
-// 		var announcementStyle = {
-// 			border-style: solid,
-// 			background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('http://cdn.jamieoliver.com/recipe-database/oldImages/xtra_med/1235_1_1436889055.jpg'),		
-// 			background-repeat: no-repeat,
-// 			background-size: cover
-// 		};
-// 		return (
-// 			<div style={announcementStyle}>
-// 				{this.props.children}
-// 			</div>
-// 		);
-// 	}
-// });
+var Content = React.createClass({
+	render: function() {
+		var contentStyle = {
+			'color': 'white',
+			'text-align': 'center'
+		};
 
-ReactDOM.render(titleAndContent, document.getElementById("announcement"));
+		return (
+				<div style={contentStyle}>
+					<h1 id="title">{this.props.data.title}</h1>
+					<p id="content" span dangerouslySetInnerHTML={{ __html: this.props.data.content}} />
+				</div>
+	)}
+});
+
+ReactDOM.render(<Announcement data={announcementData}/>, document.getElementById("announcement"));
